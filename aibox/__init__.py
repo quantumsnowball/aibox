33a93ca154093e0bde5a4ead9f9182d93d61126c
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-01 02:24:42 +0800

    comments

--- aibox/__init__.py
+++ aibox/__init__.py
@@ -12,11 +12,18 @@ def show_backward_graph(output: torch.Tensor,
                         figsize: tuple[float, float] = (12, 9),
                         dpi: str = '600',
                         **make_dot_kwargs) -> None:
+    # auto get named parameters from net
     params = dict(net.named_parameters()) if net else None
+
+    # create the Digraph
     dot = make_dot(output, params=params, **make_dot_kwargs)
     dot.attr(dpi=dpi)
+
+    # create bytes io for the graph
     graph_bytes = BytesIO(dot.pipe(format='png'))
     image = PIL.Image.open(graph_bytes)
+
+    # show the graph using matplotlib
     plt.figure(figsize=figsize)
     plt.imshow(image)
     plt.axis('off')
